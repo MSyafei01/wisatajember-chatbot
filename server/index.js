08e93ae87ce7888aa0b508dc8374ac507e3fb8e0
MSyafei <mochamadsyafei2701@gmail.com>
2026-04-18 22:52:30 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import express from "express";
 
 dotenv.config();
 
